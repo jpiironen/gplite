0ae45ce7b5a09a1556e31edaae2db62e17bc8756
Juho Piironen <juho@cai.fi>
2019-08-21 14:43:43 +0300

    one TODO.

--- R/laplace.R
+++ R/laplace.R
@@ -10,7 +10,7 @@ approx_laplace <- function(gp, K, y, fhat_old, ...) {
   z <- pobs$z
   V <- pobs$var
   n <- length(z)
-  K_chol <- t(chol(K))
+  K_chol <- t(chol(K)) # TODO: this might be numerically unstable, but perhaps this is not needed, since it cancels out when computing marginal likelihood?
   C_chol <- t(chol(K+diag(V)))
   fhat_new <- K %*% backsolve(t(C_chol), forwardsolve(C_chol, z)) 
   
@@ -48,5 +48,32 @@ approx_laplace_iterated <- function(gp, K, y, maxiter=100, tol=1e-4, ...) {
   return(approx)
 }
 
+
+
+
+
+
+
+approx_linearized_laplace <- function(gp, z, y, fhat_old, ...) {
+  
+  # calculate first the new estimate for posterior mean for f
+  pobs <- get_pseudodata(gp$lik, fhat_old, y, ...)
+  z <- pobs$z
+  V <- pobs$var
+  n <- length(z)
+  K_chol <- t(chol(K))
+  C_chol <- t(chol(K+diag(V)))
+  fhat_new <- K %*% backsolve(t(C_chol), forwardsolve(C_chol, z)) 
+  
+  # compute the log marginal likelihood
+  K_logdet <- 2*sum(log(diag(K_chol)))
+  C_logdet <- 2*sum(log(diag(C_chol)))
+  V_logdet <- sum(log(V))
+  aux <- forwardsolve(K_chol, fhat_new)
+  log_prior <- -0.5*n*log(2*pi) - 0.5*K_logdet - 0.5*sum(aux^2)
+  log_lik <- get_loglik(gp$lik, fhat_new, y, ...) 
+  log_evidence <- 0.5*n*log(2*pi) + 0.5*(K_logdet - C_logdet + V_logdet) + log_prior + log_lik
+  list(fmean=fhat_new, K_chol=K_chol, C_chol=C_chol, log_evidence=log_evidence)
+}
 
 
