103e11360dab1cefc6b479a29598184ecb964dc2
Juho Piironen <juho@cai.fi>
2020-09-17 16:53:50 +0300

    Disable argument 'method' in gp_optim

--- R/gp_optim.R
+++ R/gp_optim.R
@@ -1,24 +1,22 @@
 #' Optimize hyperparameters of a GP model
 #' 
 #' This function can be used to optimize the hyperparameters of the model to the maximum
-#'  marginal
-#' likelihood solution (type-II maximum likelihood) based on the Laplace approximation.
+#' marginal likelihood (or maximum marginal posterior if priors are used), using Nelder-Mead
+#' algorithm.
 #' 
 #' @param gp The gp model object to be fitted.
 #' @param x n-by-d matrix of input values (n is the number of observations and d the input
-#'  dimension). 
-#' Can also be a vector of length n if the model has only a single input.
+#'  dimension). Can also be a vector of length n if the model has only a single input.
 #' @param y Vector of n output (target) values.
-#' @param method Optimization method that will be passed to \code{\link{optim}} function.
-#' @param tol Relative change in the objective function value below the optimization is
+#' @param tol Relative change in the objective function value after which the optimization is
 #'  terminated. 
 #' @param maxiter Maximum number of iterations.
 #' @param verbose If TRUE, then some information about the progress of the optimization is
 #'  printed to the console.
-#' @param warnings Whether to print out some potential warnigns (such as maximum number of 
+#' @param warnings Whether to print out some potential warnings (such as maximum number of 
 #' iterations reached) during the optimization.
-#' @param ... Further arguments to be passed to \code{\link{gp_fit}} that are needed in the fitting
-#' process, for example \code{trials} in the case of binomial likelihood.
+#' @param ... Further arguments to be passed to \code{\link{gp_fit}} that are needed 
+#' in the fitting process, for example \code{trials} in the case of binomial likelihood.
 #'
 #'
 #' @return An updated GP model object.
@@ -49,8 +47,7 @@
 #'
 #' 
 #' @export
-gp_optim <- function(gp, x, y, method='Nelder-Mead', tol=1e-4, 
-                     maxiter=500, verbose=T, warnings=T, ...) {
+gp_optim <- function(gp, x, y, tol=1e-4, maxiter=500, verbose=T, warnings=T, ...) {
 
   iter <- 0
   energy <- function(param) {
@@ -65,7 +62,7 @@ gp_optim <- function(gp, x, y, method='Nelder-Mead', tol=1e-4,
   param0 <- get_param(gp)
   control <- list(reltol = tol, fnscale = length(y), maxit = maxiter, 
                   warn.1d.NelderMead = warnings)
-  res <- stats::optim(param0, energy, method = method, control = control)
+  res <- stats::optim(param0, energy, method = 'Nelder-Mead', control = control)
   if (res$convergence == 1 && warnings)
     warning('Maximum number of iterations reached, the optimization may not have converged.')
   param <- res$par
